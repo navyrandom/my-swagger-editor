a96683f5b74972ccdb444bd3690dd56da9cf26eb
navyrandom <78206604+navyrandom@users.noreply.github.com>
2021-09-30 11:08:11 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+import SwaggerEditor, {plugins} from 'swagger-editor';
+import 'swagger-editor/dist/swagger-editor.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    this.saveContent = this.saveContent.bind(this);
+  }
+
+  componentDidMount() {
+    window.editor = SwaggerEditor({
+      dom_id: '#swagger-editor',
+      layout: 'EditorLayout',
+      plugins: Object.values(plugins),
+      swagger2GeneratorUrl: 'https://generator.swagger.io/api/swagger.json',
+      oas3GeneratorUrl: 'https://generator3.swagger.io/openapi.json',
+      swagger2ConverterUrl: 'https://converter.swagger.io/api/convert',
+  });
+  }
+
+  saveContent() {
+    const content = window.localStorage.getItem('swagger-editor-content');
+    console.log(content);
+  }
+
+  render() {
+    return (
+      <div>
+        <button color="blue" onClick={this.saveContent}>
+          Save Content
+        </button>
+        <div id='swagger-editor'></div>
+      </div>
+    );
+  }
+}
+
+export default App;
